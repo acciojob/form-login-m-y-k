c3921c04ef706dc17744bbebaa6a9cf5948177ae
Mohammad Yusuf Khan <98419143+m-y-k@users.noreply.github.com>
2023-06-20 17:33:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+	const name = document.getElementsByTagName("input");
+	// name[0].Id = 'fname';
+	// name[1].Id = 'lname';
+	// console.clear();
+	// console.log(name[0].value, name[1].value);
+	alert(name[0].value + " " + name[1].value);
 }
